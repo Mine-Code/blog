cf2ad9bba488c9eb52426f64c4baf41936bad2a6
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-20 17:42:32 +0900

    feat: 簡易的なGET /api/postsを実装

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from db import db
+
+from models.posts import Post
 
 app = FastAPI()
 
@@ -8,3 +11,9 @@ async def test():
       "message": "hello world"
   }
   return response
+
+@app.get("/api/posts")
+async def get_posts():
+  session = db.get_db()
+  posts = session.query(Post).all()
+  return posts
