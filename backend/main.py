3b0de3ae90297ae6365f6b0d6147833cf5d46fb1
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-20 17:14:25 +0900

    refactor: ダブルクオーテーションに変更

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/api/test')
+@app.get("/api/test")
 async def test():
   response = {
-      'message': 'hello world'
+      "message": "hello world"
   }
   return response
