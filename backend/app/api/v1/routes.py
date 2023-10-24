1fac40a272126cf4ff05489cb075e11a65ae3352
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-24 09:47:17 +0900

    feat: api/v1/user routing

--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
 from .endpoints.test import router as test_router
+from .endpoints.user import router as user_router
 
 routers = APIRouter()
-router_list = [test_router]
+router_list = [test_router, user_router]
 
 for router in router_list:
   router.tags = routers.tags.append("v1")
